d36b7951fda4324800f2420118a942f4fd33bb83
obscuredc <77344064+obscuredc@users.noreply.github.com>
2021-09-19 08:35:16 -0400

    Update common.js

--- common.js
+++ common.js
@@ -9,3 +9,26 @@ const opener = {
         }
     }
 }
+function m(s) {
+    var t = [];
+    for(i = 0; i < s.length; i++) {
+        var r="";
+        for(i2 = 0; i2 < i; i2++) {
+            r+=s[i2];
+        }
+        t.push(r);
+        r="";
+    }
+    t.push(s);
+    return t;
+}
+var anim = m("well, goodbye!");
+var nts = document.getElementsByClassName("neon-text");
+var ticker = 0;
+var x = setInterval(function() {
+for(i = 0; i < nts.length; i++) {
+    nts[i].innerHTML=anim[ticker];
+}
+ticker++;
+ticker = ticker % anim.length;
+}, 500);
